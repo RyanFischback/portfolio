6c1e205a84b3d6da0f2c30f849d950f50f538c95
RyanFischback <ryan@vgen.co>
2026-02-07 11:56:18 -0500

    move work higher

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -54,8 +54,8 @@ const Main = () => {
             <Greeting />
             <Skills />
             <StackProgress />
-            <Education />
             <WorkExperience />
+            <Education />
             <StartupProject />
             <Projects />
             <Achievement />
